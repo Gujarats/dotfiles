0c6c1133b3e553e395da675f690d1a2415d28011
Gujarat Santana <gujaratsantana@192.168.1.109>
2022-10-13 21:10:11 +0700

    migrate to zsh
    executable
    
    neat to use stow
    
    use nix to install dependencies
    
    set homebrew and set vundle installation path
    
    home brew manual
    
    vim copy with new line
    
    get rid off antybody use oh-my-zsh instead
    
    fix stow and rename folder
    
    update readme
    
    add install oh-my-zsh command
    
    fix install and zsh to make executable nix bin
    
    add visual code studio
    
    add golang version manger
    
    anaconda removal
    
    color scheme, tmux config, zsh theme
    
    remove visual code studio due to ubuntu
    
    mapping ctrl with leader to move between pane
    
    alias git
    
    copy paste
    
    copy paste all line
    
    visual model key map
    
    remove set
    
    tmux plugin manager
    
    semoe changes
    
    add npm install
    
    continue on error
    
    set -e on top
    
    ignore some errors
    
    fix p10k error
    
    just put stow at the end
    
    trave
    
    tmux yank and save session continuum
    
    good place
    
    add get-rev

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
 # Vim setup editor setup using vim and tmux
 I used this setup for my daily usage
 
-## Installation tmux 
-I'm using ubuntu so to install type `sudo apt-get install tmux`
-
-## Installation vim and theme
-1. install vim [vundle](https://github.com/VundleVim/Vundle.vim) 
-2. install [solarized](https://github.com/altercation/vim-colors-solarized)
-3. create `.vimrc` file if haven't created `touch ~/.vimrc`, and copy paste my .vimrc file to yours
-4. and also I'm using base16 to theme my terminal and vim checkout this out [base16Shell](https://github.com/chriskempson/base16-shell) and [base16Vim](https://github.com/chriskempson/base16-vim)
+## Installation 
+reference : https://nixos.org
+just run `install.sh` and make sure everything is run smoothly
 
 ## For YouCompleteme
 1. sudo apt-get install cmake
 2. sudo apt-get install python-dev
 3. ./install.py --clang-completer
-
-## Environtment Development
-I used this setup for my daily activity work. these are the language that used `go` , `perl` , `javascript` , `php`
-
-## setup NVIM for .vimrc
- - copy-paste .vimrc to your home dir
- - copy-paste nvim/init.vim to ~/.config/nvim/
- - source ~/.vimrc
- - :PluginInstall
- - this should be sufficient to be editor
-
-NOTE :  Tested on Ubuntu 16.04
---
+++ install.sh
@@ -0,0 +1,58 @@
+# ingore error continue command until finish
+set -e
+# install nix
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.gnumake \
+	nixpkgs.gcc \
+	nixpkgs.direnv \
+    nixpkgs.nodePackages.npm
+
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
+
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+stow p10k
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# symlink zsh
+rm -rf ~/.zshrc || true
+rm -rf ~/.zshrc_plugins.txt || true
+stow zsh || true
+source ~/.zshrc || true
+
+# install tmux plugin manager and its config
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+rm -rf ~/.tmux.conf
+stow tmux
+
+# install vundle nvim plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+
+# Use kitty terminal on MacOS
+[ `uname -s` = 'Darwin' ] && stow kitty
+
+# stow dotfiles
+stow kitty
+stow nvim
+stow switchControl
