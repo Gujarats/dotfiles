b121ff38fef0dbf5051f3e66fd26d2f05bdcf1cc
gujarats <gujarats@Gujarat-PC.localdomain>
2022-10-16 23:15:07 +0700

    init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,19 @@
+print('lua.init loaded')
+require('gujarats.base')
+require('gujarats.highlights')
+require('gujarats.plugins')
+require('gujarats.maps')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('gujarats.macos')
+end
+if is_win then
+  require('gujarats.windows')
+end
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
