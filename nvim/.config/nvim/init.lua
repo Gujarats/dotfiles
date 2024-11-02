7a953de2e30c0f8e0041d3971575a11726f5e8d8
Gujarat Santana <gujarat.santana@gmail.com>
2024-11-02 21:04:34 +0700

    live grep for searching hidden files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("Copilot disable")
+
+require("telescope").setup({
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+    },
+    grep_string = {
+      additional_args = { "--hidden" },
+    },
+    live_grep = {
+      additional_args = { "--hidden" },
+    },
+  },
+})
