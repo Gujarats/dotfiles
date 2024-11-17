20e0b228308d7532420b9e1cba1cad43ee169142
Gujarat Santana <gujarat.santana@gmail.com>
2024-11-17 08:13:43 +0700

    remove plugin manager use lazyvim instead

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ stow tmux
 # install vundle nvim plugin manager
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 #Packer nvim plguin manager
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Use kitty terminal on MacOS
 [ $(uname -s) = 'Darwin' ] && stow kitty
