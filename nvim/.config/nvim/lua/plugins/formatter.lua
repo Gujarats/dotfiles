6539ee30bc98966837b5af4b819374bfb2e419bd
Gujarat Santana <gujarat.santana@gmail.com>
2024-11-21 20:54:56 +0700

    plugin removal for go html, use .prettier and npm module

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,12 +1,5 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["html"] = { "prettier" },
-        ["gohtml"] = { "prettier", "prettierd" },
-        ["gohtmltmpl"] = { "prettier", "prettierd" },
-      },
-    },
   },
 }
