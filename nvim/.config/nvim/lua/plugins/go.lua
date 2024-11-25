bd5230be83ca7d831961d2540c56ab661998ba65
Gujarat Santana <gujarat.santana@gmail.com>
2024-11-25 19:31:23 +0700

    enable ray-x/navigator on specific filetypes

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,9 +1,10 @@
 return {
   "ray-x/navigator.lua",
+  ft = { "go", "gomod" },
   dependencies = {
     { "hrsh7th/nvim-cmp" },
     { "nvim-treesitter/nvim-treesitter" },
-    { "ray-x/guihua.lua", run = "cd lua/fzy && make" },
+    { "ray-x/guihua.lua",               run = "cd lua/fzy && make" },
     {
       "ray-x/go.nvim",
       event = { "CmdlineEnter" },
@@ -37,9 +38,9 @@ return {
         vim.api.nvim_buf_set_keymap(0, "n", "<C-c>", ":GoCoverage -p<CR>", {})
 
         -- Opens test files
-        vim.api.nvim_buf_set_keymap(0, "n", "A", ":lua require('go.alternate').switch(true, '')<CR>", {}) -- Test
-        vim.api.nvim_buf_set_keymap(0, "n", "V", ":lua require('go.alternate').switch(true, 'vsplit')<CR>", {}) -- Test Vertical
-        vim.api.nvim_buf_set_keymap(0, "n", "S", ":lua require('go.alternate').switch(true, 'split')<CR>", {}) -- Test Split
+        --vim.api.nvim_buf_set_keymap(0, "n", "A", ":lua require('go.alternate').switch(true, '')<CR>", {}) -- Test
+        --vim.api.nvim_buf_set_keymap(0, "n", "V", ":lua require('go.alternate').switch(true, 'vsplit')<CR>", {}) -- Test Vertical
+        --vim.api.nvim_buf_set_keymap(0, "n", "S", ":lua require('go.alternate').switch(true, 'split')<CR>", {}) -- Test Split
       end,
       group = vim.api.nvim_create_augroup("go_autocommands", { clear = true }),
     })
