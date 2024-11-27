bd622bfceb81801125c7695521e49f25bfd34e3d
Gujarat Santana <gujarat.santana@gmail.com>
2024-11-27 18:00:01 +0700

    add keymap for showing all functions

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -33,16 +33,6 @@ keymap.set("", "s", "") -- override s to do nothing
 --copy current row
 keymap.set("n", "Y", "<Esc>^v$y")
 
---vim.keymap.set("n", "<F2>", "<Cmd>NvimTreeToggle<CR>", {})
---keymap.set("n", "<F3>", ":VTerm<CR>")
-
--- telescope ignroe command for rerence in the futrue
---keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
---keymap.set("n", "<leader>f", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
---keymap.set("n", "<leader>f", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
---keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
---keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-
 -- GitSigns
 keymap.set("n", "]n", "<cmd>Gitsigns next_hunk<cr>") -- go to next changes git
 --keymap.set("n", "]p", "<cmd>Gitsigns prev_hunk<cr>") -- go to previous changes git // Comment for now due to p use for paste
@@ -53,3 +43,7 @@ keymap.set("n", "]U", "<cmd>Gitsigns reset_buffer<cr>") -- undo changes whole bu
 keymap.set("n", "\\sp", function()
   require("gujarats.file").print_full_path()
 end, { expr = true, desc = "print the current buffer file path" })
+
+keymap.set("n", "\\se", function()
+  require("telescope.builtin").lsp_document_symbols({ symbols = "function" })
+end, { desc = "show all functions in current buffer" })
